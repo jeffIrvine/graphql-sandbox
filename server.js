a993100fd4253d85e7e37c97b872d39d52456bdf
jeffIrvine <jirvine1212@gmail.com>
2021-05-26 10:43:26 -0700

    added author mutation

--- server.js
+++ server.js
@@ -94,6 +94,21 @@ const RootMutationType = new GraphQLObjectType({
         books.push(book)
         return book
       }
+    },
+    addAuthor: {
+      type: AuthorType,
+      description: 'Adds an Author',
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve: (parent, args) => {
+        const author = { 
+          id: authors.length + 1, 
+          name: args.name,
+        }
+        authors.push(author)
+        return author
+      }
     }
   })
 })
