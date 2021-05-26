9d03b718a0908913ea69687a02aaca118e098ec3
jeffIrvine <jirvine1212@gmail.com>
2021-05-25 20:27:32 -0700

    running graphql gui on server

--- server.js
+++ server.js
@@ -2,16 +2,27 @@ const express = require('express')
 const expressGraphQL = require('express-graphql').graphqlHTTP
 const {
   GraphQLSchema,
-  GraphQLObjectType
+  GraphQLObjectType,
+  GraphQLString
 } = require('graphql')
 const app = express()
 
 const schema = new GraphQLSchema({
-  
+  query: new GraphQLObjectType({
+    name: 'Helloworld',
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => 'Hello World'
+      }
+    })
+  })
 })
 
 app.use('/graphql', expressGraphQL({
+  schema: schema,  
   graphiql: true
+
 }))
 app.listen(8000., () => console.log('server is running '))
 
