2673431b6c3870fba588642562d54101b4e812bd
jeffIrvine <jirvine1212@gmail.com>
2021-05-25 20:22:29 -0700

    got server to run by adding .graphqlHTTP to express-graphql

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+const {
+  GraphQLSchema,
+  GraphQLObjectType
+} = require('graphql')
 const app = express()
+
+const schema = new GraphQLSchema({
+  
+})
+
+app.use('/graphql', expressGraphQL({
+  graphiql: true
+}))
 app.listen(8000., () => console.log('server is running '))
+
